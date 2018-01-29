584c632362c7e3361a00b6417927727be79be596
Fenton Travers <fenton.travers@gmail.com>
2018-01-29 13:27:43 +0700

    clean up so can library-ize

--- src/cljs/sample_reagent/channels.cljs
+++ src/cljs/sample_reagent/channels.cljs
@@ -1,55 +1,81 @@
 (ns sample-reagent.channels
-  (:require-macros [cljs.core.async.macros :refer [go go-loop alt!]])
+  (:require-macros [cljs.core.async.macros :refer [go go-loop]])
   (:require
-   [cljs.core.async :as async :refer [put! chan <! >! timeout close! mult tap]]))
+   [cljs.core.async :as async]))
 
-(def input-channel (chan))
+(defn setup-channels [pub-fn]
+  "The function creates three artifacts: 
 
-(defn print-chan [label ch]
-  (go-loop []
-    (.log js/console label (str (<! ch)))
-    (recur)))
+1. write-to-chan: This is the channel to which you should write all
+your messages into.
 
-(defn duplicate-output-channel []
-  (let [m (mult input-channel)
-        c1 (chan)
-        c2 (chan)]
-    (tap m c1)
-    (tap m c2)
-    [c1 c2]))
+2. tap-me-mult: This is the mult that you should tap with any
+additional channels that you'd like to create a clone of the
+write-to-chan.
 
-(defn test-duplicate-output-channel []
-  (let [[ch1 ch2] (duplicate-output-channel)]
-    (print-chan "channel 1" ch1 )
-    (print-chan "channel 2" ch2 ))
-  (go
-    (dotimes [n 10]
-      (>! input-channel (str "Blah: " n)))))
+3. a-pub-queue: publication queue that is created conceptually on the
+write-to-chan with the supplied pub-fn. "
+  (let [write-to-ch (async/chan)
+        tap-me-mult (async/mult write-to-ch)
+        pub-ch (async/chan)
+        _ (async/tap tap-me-mult pub-ch)
+        a-pub-queue (async/pub pub-ch pub-fn)]
+    [write-to-ch tap-me-mult a-pub-queue]))
 
-(defn pub-sub [pub-ch msg-type]
-  (let [pub-queue (async/pub pub-ch :msg-type)
-        cats-ch (chan)]
-    (async/sub pub-queue msg-type cats-ch)
-    cats-ch))
+(defn sub-to-pub [pub-fn-val a-pub-queue]
+  "Given a publication queue, a-pub-queue, and a value, pub-fn-val,
+  that the pub-fn can evaluate a message to, return a channel that
+  those messages will be sent to."
+  (let [output-ch (async/chan)]
+    (async/sub a-pub-queue pub-fn-val output-ch)
+    output-ch))
 
-(defn feed [msg]
-  (go (>! input-channel msg)))
+(comment
+  (defn print-channel [label ch]
+    "Print everything that comes over channel 'ch'"
+    (go-loop []
+      (.log js/console label (str (async/<! ch)))
+      (recur)))
 
-(defn read-evry [ch]
-  (go-loop []
-    (.log js/console "everything channel: " (str (<! ch)))
-    (recur)))
+  (defn send-msg [msg write-to-channel]
+    "Send a message 'msg' on channel 'write-to-channel'."
+    (go (async/>! write-to-channel msg)))
 
-(defn test-pub-sub []
-  (let [[pub-ch everything-ch] (duplicate-output-channel)
-        cats-ch (pub-sub pub-ch :cats)]
-    (print-chan "cats channel: " cats-ch)
-    everything-ch
-    ))
+  (defn send-some-testdata [input-ch]
+    (send-msg "abc123" input-ch)
+    (send-msg {:msg-type :dogs :disposition "friendly"} input-ch)
+    (send-msg {:msg-type :cats :disposition "aloof"} input-ch))
+  
+  (defn test-channels []
+    (let [
+          ;; create a publication-queue that calls the function
+          ;; ':msg-type' on messages that get written to 'write-to-ch'
+          [write-to-ch tap-me-mult msg-type-pub] (setup-channels :msg-type)
 
-(defn gg []
-  (feed "abc123")
-  (feed {:msg-type :dogs :disposition "friendly"})
-  (feed {:msg-type :cats :disposition "aloof"})
-  (dotimes [n 5]
-    (feed (str "Blah: " n))))
+          ;; make a channel that receives messages that the following
+          ;; expression evaluates to true for:
+          ;; 
+          ;; (= :dogs (:msg-type msg))
+          dogs-ch (sub-to-pub :dogs msg-type-pub)
+
+          ;; make a channel that receives messages that the following
+          ;; expression evaluates to true for:
+          ;; 
+          ;; (= :cats (:msg-type msg))
+          cats-ch (sub-to-pub :cats msg-type-pub)
+
+          all-ch (async/chan)]
+
+      ;; 'all-ch' will now receive all messages that get written to
+      ;; 'write-to-ch'
+      (async/tap tap-me-mult all-ch)
+
+      (print-channel "Everything Channel: " all-ch)
+      (print-channel "Dogs Channel: " dogs-ch)
+      (print-channel "Cats Channel: " cats-ch)
+
+      write-to-ch))
+
+  (def write-ch (test-channels))
+
+  (send-some-testdata write-ch))
